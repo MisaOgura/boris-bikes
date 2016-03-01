c792acc09a3c0ba74e0ea02ef622e95362eeff7d
Tobenna <tobennandu@gmail.com>
2016-03-01 16:22:11 +0000

    Error messages for full Stations

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,22 +1,29 @@
-require_relative "bike"
+require_relative 'bike'
 
 class DockingStation
-
+  attr_reader :capacity
   def initialize
+    @capacity = 20
     @bikes_array = [Bike.new]
     @bike = Bike.new
   end
 
   def release_bike
-    raise "Docking station is EMPTY!!!" if self.bikes == 0
+    raise 'Docking station is EMPTY!!!' if bikes == 0
     @bike
   end
 
-  def dock bike
+  def dock(bike)
+    raise 'Station is full' if is_full?
     @bikes_array << bike
     bike
   end
+
   def bikes
     @bikes_array.length
+  end
+
+  def is_full?
+    bikes == @capacity
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,25 +1,32 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+  it { is_expected.to respond_to(:release_bike) }
 
-  it {is_expected.to respond_to(:release_bike)}
-
-  it "returns a working bike" do
+  it 'returns a working bike' do
     my_bike = subject.release_bike
     expect(my_bike).to be_working
   end
 
-  it "Returns the number of bikes docked" do
+  it 'Returns the number of bikes docked' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
 
-  it {is_expected.to respond_to(:dock).with(1).argument}
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it "raises" do
+  it 'raises' do
     if subject.bikes == 0
-      expect {subject.release_bike}.to raise_error("Docking station is EMPTY!!!")
+      expect { subject.release_bike }.to raise_error('Docking station is EMPTY!!!')
     end
   end
-
+  describe '#dock' do
+    it 'raises' do
+      19.times do
+        bike = Bike.new
+        subject.dock bike
+      end
+      expect { subject.dock Bike.new }.to raise_error('Station is full') if subject.is_full?
+    end
+  end
 end
