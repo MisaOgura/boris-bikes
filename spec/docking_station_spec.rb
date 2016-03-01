9adbe11a7a2ad00743fbf46453066423fddaac91
Tobenna <tobennandu@gmail.com>
2016-03-01 18:23:58 +0000

    Mimmicking function behaviours using doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,22 +1,21 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+  let(:bike) { double :bike }
   it { is_expected.to respond_to(:release_bike) }
 
   it 'returns a working bike' do
-    subject.dock double(:bike)
-    my_bike = subject.release_bike
-    expect(my_bike).to be_working
+    bike = double('bike', :working? => true)
+    subject.dock bike
+    expect(bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   describe '#release_bike' do
     it 'raises' do
-      bike = double(:bike)
-      bike.is_working = false
       station = DockingStation.new
-      station.dock bike
+      station.dock double(:is_working => false)
       expect { station.release_bike }.to raise_error('Cannot release a broken bike')
     end
   end
@@ -29,10 +28,9 @@ describe DockingStation do
   describe '#dock' do
     it 'raises' do
       20.times do
-        bike = double(:bike)
         subject.dock bike
       end
-      expect { subject.dock double(:bike) }.to raise_error('Station is full') if subject.bikes == subject.capacity
+      expect { subject.dock bike }.to raise_error('Station is full') if subject.bikes == subject.capacity
     end
   end
 
