609ef538f5c4b90ac614b6a5d53c616f852fa7b7
Tobenna <tobennandu@gmail.com>
2016-03-01 15:29:59 +0000

    Added dock functionality to the DockingStation Class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,17 @@
 require_relative "bike"
 
 class DockingStation
-
+  def initialize
+    @bikes = []
+  end
   def release_bike
     Bike.new
   end
-
+  def dock bike
+    @bikes << bike
+    bike
+  end
+  def bikes
+    @bikes.length
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,10 @@ describe DockingStation do
     expect(my_bike).to be_working
   end
 
+  it "Returns the number of bikes docked" do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(station.dock(bike)).to eq bike
+  end
+  it {is_expected.to respond_to(:dock).with(1).argument}
 end
