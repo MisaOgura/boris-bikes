3f18b39a4c2cbafbf7a1da70e053fa1e7eaeabb8
Misa Ogura <misaogura@Misas-MacBook-Pro.local>
2016-03-01 13:43:46 +0000

    Added Bike class and spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,16 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+
+  it {is_expected.to respond_to(:release_bike)}
+
   before do
     @docking_station = DockingStation.new
+    @bike = @docking_station.release_bike
   end
 
-  it {expect(@docking_station).to respond_to(:release_bike)}
+  it "returns a bike" do
+    expect(@bike.class).to eq("Bike")
+  end
 
 end
